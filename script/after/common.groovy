8dc230645b3c85eb6fd07c44ee11d723f39685c7
Aldaris <aldarisx@outlook.com>
2021-11-19 17:29:36 +0800

    add 302时输出

--- script/after/common.groovy
+++ script/after/common.groovy
@@ -11,10 +11,14 @@ CloseableHttpResponse response = getProperty("response")
 
 if (HttpStatus.SC_OK == response.getStatusLine().getStatusCode()) {
     // 200时打印结果
+    println("head:${response.getAllHeaders()}")
     var responseStr = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8)
     println("response: " + responseStr)
+} else if (HttpStatus.SC_TEMPORARY_REDIRECT == response.getStatusLine().getStatusCode()) {
+    println("head:${response.getAllHeaders()}")
 } else {
     // 非200时输出状态
+    println("head:${response.getAllHeaders()}")
     println("status: " + response.getStatusLine())
 }
 
